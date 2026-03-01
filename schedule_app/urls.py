0957b59406682116ee7023defefdb72c2a0e66a4
Michail19 <Mi.ershov.2017@yandex.ru>
2026-03-01 13:02:49 +0300

    Add employee

--- schedule_app/urls.py
+++ schedule_app/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import WeeklyScheduleView, UpdateScheduleView
+from .views import WeeklyScheduleView, UpdateScheduleView, AddEmployeeView
 
 urlpatterns = [
     path("weekly/", WeeklyScheduleView.as_view()),
     path("update/<int:employee_id>/", UpdateScheduleView.as_view()),
+    path("add/", AddEmployeeView.as_view()),
 ]
--- schedule_app/views.py
+++ schedule_app/views.py
@@ -1,9 +1,11 @@
 from datetime import datetime
 from django.utils.dateparse import parse_date
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework import status
 from .models import Employee
+from django.contrib.auth import get_user_model
 from .serializers import EmployeeDetailsSerializer
 from .serializers import WeekScheduleSerializer
 from .services import get_full_schedule_for_week
@@ -54,3 +56,30 @@ class UpdateScheduleView(APIView):
             return Response(serializer.data)
 
         return Response(serializer.errors, status=400)
+
+
+User = get_user_model()
+
+
+class AddEmployeeView(APIView):
+    permission_classes = [IsAdminUser]
+
+    def post(self, request):
+        fio = request.data.get("fio")
+        username = request.data.get("username")
+        password = request.data.get("password")
+
+        user = User.objects.create_user(
+            username=username,
+            password=password
+        )
+
+        employee = Employee.objects.create(
+            fio=fio,
+            user=user
+        )
+
+        return Response({
+            "id": employee.id,
+            "fio": employee.fio
+        }, status=status.HTTP_201_CREATED)
